d4b7eb1e40c13dd7951d7301c6e95893048c0d46
MANSI UNGE <mansiunge@gmail.com>
2026-02-11 10:02:03 +0530

    day 2 : program to check year is leap or not

---
+++ d2_problem_1.py
@@ -0,0 +1,8 @@
+year = int(input( "enter the year : "))
+
+print(f"the year you entered is : {year}")
+
+if (year  % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+	print(" year is leap year")
+else : 
+    print("year is not leap year")
